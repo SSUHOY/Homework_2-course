6e9b669d03cb54a1a5fe9995bf828b98570c0ed3
SSUHOY <sammysoul96@gmail.com>
2023-02-18 15:58:51 +0700

    добавил alert Привет, ИМЯ !

--- hw-N.js
+++ hw-N.js
@@ -1,11 +1,2 @@
-let user = {
-    name: "John",
-    age: 35,
-    isAdmin: true,
-    cityOfResidence: "Russia"
-}
-
-delete user.cityOfResidence;
-
-let info = prompt('Какую информацию хотите узнать о пользователе?')
-info = alert(`${user}`)
+let yourName = prompt('Как Вас зовут?');
+alert(`Привет, ${yourName} !`);
