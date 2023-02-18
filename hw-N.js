05bfbf693eb99bb910f9e3434c343efd2dd3b2a6
SSUHOY <sammysoul96@gmail.com>
2023-02-18 22:54:55 +0700

    Прописал код для отправки в виде архива ДЗ

--- hw-N.js
+++ hw-N.js
@@ -1,2 +1,25 @@
+const year = ('Первый IPhone вышел в 2007 году');
+alert(year);
+
+const author = ('Java Script был создан Бренданом Эйхом');
+alert(author);
+
+
+let user = {
+    name: "John",
+    age: 29,
+    isAdmin: true,
+    cityOfResidence: "Russia"
+}
+
+alert(user);
+
+delete user.cityOfResidence;
+
+let info = prompt('Какую информацию хотите узнать о пользователе?')
+info = alert(`${user}`);
+
 let yourName = prompt('Как Вас зовут?');
 alert(`Привет, ${yourName} !`);
+
+
